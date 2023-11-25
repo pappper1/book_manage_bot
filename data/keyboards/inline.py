aa8b743de26d3a5d3f6fe99eed0d62e0e08788ba
pappper1 <rewkruterbyby@gmail.com>
2023-11-25 20:20:38 +0100

    Добавлено создание новое книги

--- data/keyboards/inline.py
+++ data/keyboards/inline.py
@@ -9,3 +9,33 @@ async def start():
 
 	ikb.add(list_books_button).add(find_book_button, create_book_button)
 	return ikb
+
+async def back(bot_page: str):
+	ikb = InlineKeyboardMarkup(row_width=1)
+
+	back_button = InlineKeyboardButton(text='🔚 Назад', callback_data=f"back_{bot_page}")
+
+	ikb.add(back_button)
+	return ikb
+
+async def book_categories(bot_page: str, categories, current_page: int):
+	ikb = InlineKeyboardMarkup(row_width=3)
+	categories_on_page = 6
+	pages = (len(categories) // categories_on_page) if len(categories) <= categories_on_page \
+		else (len(categories) // categories_on_page)+1
+	next_page = (current_page + 1) if current_page != pages else 1
+	previous_page = (current_page - 1) if current_page != 1 else pages
+
+	previous_page_button = InlineKeyboardButton(text='Предыдущая', callback_data=f"goto_{previous_page}")
+	current_page_button = InlineKeyboardButton(text=f"{current_page}", callback_data='selected')
+	next_page_button = InlineKeyboardButton(text='Следующая', callback_data=f"goto_{next_page}")
+	back_button = InlineKeyboardButton(text='🔚 Назад', callback_data=f"back_{bot_page}")
+
+	page = current_page - 1
+	categories_for_page = categories[page*categories_on_page:categories_on_page*current_page]
+	for category in categories_for_page:
+		ikb.add(InlineKeyboardButton(text=f"{category['category_title']}",
+		                             callback_data=f"category_{category['category_title']}"))
+
+	ikb.add(previous_page_button, current_page_button, next_page_button).add(back_button)
+	return ikb
--- handlers/create_book.py
+++ handlers/create_book.py
@@ -1,1 +1,93 @@
-from loader import dp
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from loader import dp, db
+import data.keyboards.inline as ikb
+from data.states import NewBook
+from data.media.texts import *
+
+
+@dp.callback_query_handler(text='create_book')
+async def create_book(call: types.CallbackQuery, state: FSMContext):
+	await NewBook.title.set()
+
+	await call.message.edit_text(text=new_book_title_text, reply_markup=await ikb.back(bot_page='start'))
+
+
+@dp.message_handler(state=NewBook.title)
+async def new_book_title(message: types.Message, state: FSMContext):
+	title = message.text
+	if len(title) > 256:
+		await message.answer(text=max_lenght_text, reply_markup=await ikb.back(bot_page='start'))
+
+	else:
+		await NewBook.author.set()
+		await state.update_data(title=title)
+
+		await message.answer(text=new_book_author_text, reply_markup=await ikb.back(bot_page='start'))
+
+
+@dp.message_handler(state=NewBook.author)
+async def new_book_author(message: types.Message, state: FSMContext):
+	author = message.text
+	if len(author) > 256:
+		await message.answer(text=max_lenght_text, reply_markup=await ikb.back(bot_page='start'))
+
+	else:
+		await NewBook.description.set()
+		await state.update_data(author=author)
+
+		await message.answer(text=new_book_description_text, reply_markup=await ikb.back(bot_page='start'))
+
+
+@dp.message_handler(state=NewBook.description)
+async def new_book_description(message: types.Message, state: FSMContext):
+	description = message.text
+	categories = await db.get_categories()
+
+	await NewBook.category.set()
+	await state.update_data(description=description, categories=categories)
+	await message.answer(text=new_book_category_text, reply_markup=await ikb.book_categories(bot_page='start',
+                                                                                             categories=categories,
+	                                                                                         current_page=1))
+
+
+@dp.message_handler(state=NewBook.category)
+async def new_book_category_mes(message: types.Message, state: FSMContext):
+	category_title = message.text
+
+	if len(category_title) > 256:
+		await message.answer(text=max_lenght_text, reply_markup=await ikb.back(bot_page='start'))
+
+	else:
+		data = await state.get_data()
+		title, author, description = data['title'], data['author'], data['description']
+
+		await state.finish()
+		await db.add_category(title=category_title)
+		await db.add_book(title=title, author=author, description=description, category=category_title)
+		await message.answer(text=await new_book_added_text(title=title, category_title=category_title))
+		await message.answer(text=start_text, reply_markup=await ikb.start())
+
+
+@dp.callback_query_handler(state=NewBook.category)
+async def new_book_category_call(call: types.CallbackQuery, state: FSMContext):
+	data = await state.get_data()
+
+	if call.data.startswith('goto_'):
+		categories = data['categories']
+		page = int(call.data.split('goto_')[1])
+
+		await call.message.edit_reply_markup(reply_markup=await ikb.book_categories(bot_page='start',
+		                                                                            categories=categories,
+		                                                                            current_page=page))
+
+	elif call.data.startswith('category_'):
+		category = call.data.split('category_')[1]
+		title, author, description = data['title'], data['author'], data['description']
+
+		await state.finish()
+		await db.add_book(title=title, author=author, description=description, category=category)
+		await call.message.answer(text=await new_book_added_text(title=title, category_title=category))
+		await call.message.answer(text=start_text, reply_markup=await ikb.start())
+		await call.message.delete()
